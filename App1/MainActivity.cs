18d181f1a06289682d54c2f05f2bf5249be81d2e
ForTakEr <simotonu.prees@tptlive.ee>
2018-11-15 09:36:06 +0200

    Mälupulgalt giti

---
+++ App1/MainActivity.cs
@@ -0,0 +1,173 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    public class MainActivity : ListActivity
+    {
+        public static List<SocialPost> posts;
+        public static List<Comment> comments;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+
+            if(posts == null) TestPosts();
+
+            ListAdapter = new PostAdapter(this, posts);
+        }
+
+        public void TestPosts()
+        {
+            posts = new List<SocialPost>();
+            comments = new List<Comment>();
+
+            Comment Comment = new Comment
+            {
+                Name = "Karupoeg",
+                Message = "Ei kannata",
+                Likes = 228
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Bemmi vend",
+                Message = "Tahab audit",
+                Likes = 0
+            };
+            comments.Add(Comment);
+
+            SocialPost post = new SocialPost
+            {
+                Name = "Test1",
+                Message = "See sõnum on tekkinud siia ns",
+                Likes = 0,
+                Date = DateTimeOffset.FromUnixTimeSeconds(1541056855),
+                Comments = comments
+            };
+            posts.Add(post);
+
+            comments = new List<Comment>();
+
+            Comment = new Comment
+            {
+                Name = "Test1. 2post",
+                Message = "Test",
+                Likes = 3
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test2. 2post",
+                Message = "Test",
+                Likes = 5
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test3. 2post",
+                Message = "Test",
+                Likes = 9
+            };
+            comments.Add(Comment);
+
+            post = new SocialPost
+            {
+                Name = "Test2",
+                Message = "Testing message",
+                Likes = 0,
+                Date = DateTimeOffset.FromUnixTimeSeconds(1500056855),
+                Comments = comments,
+                ContainsPic = true,
+                Pic = Resource.Drawable.haters
+            };
+            posts.Add(post);
+
+            comments = new List<Comment>();
+
+            Comment = new Comment
+            {
+                Name = "Test1. 3post",
+                Message = "Test",
+                Likes = 10
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test2. 3post",
+                Message = "Test",
+                Likes = 20
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test3. 3post",
+                Message = "Test",
+                Likes = 30
+            };
+            comments.Add(Comment);
+
+            post = new SocialPost
+            {
+                Name = "Test3",
+                Message = "Message to test FACEBOOK",
+                Likes = 0,
+                Date = DateTimeOffset.FromUnixTimeSeconds(1305056855),
+                Comments = comments
+            };
+            posts.Add(post);
+
+            comments = new List<Comment>();
+
+            Comment = new Comment
+            {
+                Name = "Test1. 4post",
+                Message = "Test",
+                Likes = 10
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test2. 4post",
+                Message = "Test",
+                Likes = 10
+            };
+            comments.Add(Comment);
+
+            Comment = new Comment
+            {
+                Name = "Test3. 4post",
+                Message = "Test",
+                Likes = 10
+            };
+            comments.Add(Comment);
+
+            post = new SocialPost
+            {
+                Name = "Test4",
+                Message = "See sonum on tekkinud siia ns",
+                Likes = 0,
+                Date = DateTimeOffset.FromUnixTimeSeconds(1205056005),
+                Comments = comments,
+                ContainsPic = true,
+                Pic = Resource.Drawable.Random
+            };
+            posts.Add(post);
+        }
+    }
+}
---
+++ App1/SocialPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    public class SocialPost
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public int Likes { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public List<Comment> Comments { get; set; }
+        public string CommentNumber { get; set; }
+        public bool IsLiked { get; set; } = false;
+        public bool ContainsPic { get; set; } = false;
+        public int Pic { get; set; }
+    }
+}
